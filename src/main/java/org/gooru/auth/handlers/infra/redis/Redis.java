8c89a612dde2c1a19c85e60e2751c70ce5d9344e
Sheeban Singaram <sheeban.s@goorulearning.org>
2015-12-28 18:44:35 +0530

    changed redis client rxjava to core

--- src/main/java/org/gooru/auth/handlers/infra/redis/Redis.java
+++ src/main/java/org/gooru/auth/handlers/infra/redis/Redis.java
@@ -1,16 +1,17 @@
 package org.gooru.auth.handlers.infra.redis;
 
+import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
+import io.vertx.redis.RedisClient;
 import io.vertx.redis.RedisOptions;
-import io.vertx.rxjava.redis.RedisClient;
 
 public final class Redis {
 
   public static RedisClient client;
 
-  public void create(io.vertx.core.Vertx vertx, JsonObject config) {
+  public void create(Vertx vertx, JsonObject config) {
     if (client == null) {
-      client = RedisClient.create((io.vertx.rxjava.core.Vertx)vertx, new RedisOptions(config));
+      client = RedisClient.create(vertx, new RedisOptions(config));
     }
   }
 
