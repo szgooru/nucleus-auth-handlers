d7a13bc72917b28d30e4b2912ec37ac18f2e8d6c
Sheeban Singaram <sheeban.s@goorulearning.org>
2015-12-31 11:12:08 +0530

    added message bus response transformer

--- src/main/java/org/gooru/auth/handlers/constants/MessageConstants.java
+++ src/main/java/org/gooru/auth/handlers/constants/MessageConstants.java
@@ -10,5 +10,15 @@ public class MessageConstants {
   public static final String MSG_OP_STATUS_SUCCESS = "success";
   public static final String MSG_USER_ANONYMOUS = "anonymous";
   public static final String MSG_USER_ID = "userId";
+  public static final String MSG_HTTP_STATUS = "http.status";
+  public static final String MSG_HTTP_BODY = "http.body";
+  public static final String MSG_HTTP_RESPONSE = "http.response";
+  public static final String MSG_HTTP_ERROR = "http.error";
+  public static final String MSG_HTTP_VALIDATION_ERROR = "http.validation.error";
+  public static final String MSG_HTTP_HEADERS = "http.headers";
+  
+//Containers for different responses
+ public static final String RESP_CONTAINER_MBUS = "mb.container";
+ public static final String RESP_CONTAINER_EVENT = "mb.event";
 
 }
--- src/main/java/org/gooru/auth/handlers/processors/MessageProcessor.java
+++ src/main/java/org/gooru/auth/handlers/processors/MessageProcessor.java
@@ -1,14 +1,15 @@
 package org.gooru.auth.handlers.processors;
 
-import org.gooru.auth.handlers.constants.MessageConstants;
-import org.gooru.auth.handlers.constants.MessagebusEndpoints;
-import org.gooru.auth.handlers.processors.exceptions.InvalidRequestException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import io.vertx.core.MultiMap;
 import io.vertx.core.eventbus.Message;
 import io.vertx.core.json.JsonObject;
+
+import org.gooru.auth.handlers.constants.MessageConstants;
+import org.gooru.auth.handlers.processors.exceptions.InvalidRequestException;
+import org.gooru.auth.handlers.processors.exceptions.InvalidUserException;
+import org.gooru.auth.handlers.processors.transformers.ResponseTransformerBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MessageProcessor implements Processor {
 
@@ -17,7 +18,7 @@ public class MessageProcessor implements Processor {
   private final static String REQUEST_PARAMS = "request.params";
 
   private Message<Object> message;
-  
+
   private Command handler;
 
   public MessageProcessor(Command handler, Message<Object> message) {
@@ -27,7 +28,6 @@ public class MessageProcessor implements Processor {
 
   @Override
   public JsonObject process() {
-    JsonObject result = null;
     try {
       if (message == null || !(message.body() instanceof JsonObject)) {
         LOG.error("Invalid message received, either null or body of message is not JsonObject ");
@@ -36,11 +36,18 @@ public class MessageProcessor implements Processor {
       JsonObject data = (JsonObject) message.body();
       MultiMap headers = message.headers();
       String command = headers.get(MessageConstants.MSG_HEADER_OP);
-      result = handler.exec(command, headers, params(data), requestBody(data));
+      JsonObject result = handler.exec(command, headers, params(data), requestBody(data));
+      return new ResponseTransformerBuilder().build(result).transform();
     } catch (InvalidRequestException e) {
-
+      LOG.warn("Caught Invalid Request exception while processing", e);
+      return new ResponseTransformerBuilder().build(e).transform();
+    } catch (InvalidUserException e) {
+      LOG.warn("Caught Invalid User while processing", e);
+      return new ResponseTransformerBuilder().build(e).transform();
+    } catch (Throwable throwable) {
+      LOG.warn("Caught unexpected exception here", throwable);
+      return new ResponseTransformerBuilder().build(throwable).transform();
     }
-    return result;
   }
 
   private JsonObject params(JsonObject message) {
