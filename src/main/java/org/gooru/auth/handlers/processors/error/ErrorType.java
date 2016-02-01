747b5fdc668c77898c7564d70af1a2871a92e524
Sheeban Singaram <sheeban.s@goorulearning.org>
2016-02-01 10:42:27 +0530

    updated the readme file

--- src/main/java/org/gooru/auth/handlers/processors/error/ErrorType.java
+++ src/main/java/org/gooru/auth/handlers/processors/error/ErrorType.java
@@ -4,7 +4,7 @@ public enum ErrorType {
 
   PARAMS_INVALID("params_invalid", "Request parameters/fields were not valid."), UNKNOW_RECORD("unknown_record", "Record was not found."),
   UNKNOW_ROUTE("unknown_route", "URL was not valid."), API_ERROR("api_error", "Internal API error."), PERMISSION_ERROR("permission_error", ""),
-  UNAUTHORIZE_ERROR("unauthorize_error", "");
+  UNAUTHORIZE_ERROR("authorization_error", "");
 
   private String name;
 
