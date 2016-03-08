51da985fcd65ba8700cbe1bffc6babe1bf292bd9
Sheeban Singaram <sheeban.s@goorulearning.org>
2016-03-08 15:06:15 +0530

    removed unused the event enum types

--- src/main/java/org/gooru/nucleus/auth/handlers/processors/event/Event.java
+++ src/main/java/org/gooru/nucleus/auth/handlers/processors/event/Event.java
@@ -3,8 +3,7 @@ package org.gooru.nucleus.auth.handlers.processors.event;
 public enum Event {
 
   CREATE_USER("event.user.create"), UPDATE_USER("event.user.update"), UPDATE_USER_PREFS("event.user.prefs.update"), UPDATE_USER_EMAIL_CONFIRM(
-          "event.user.update.email.confirm"), RESEND_CONFIRM_EMAIL("event.user.resend.confirm.email"), UPDATE_USER_EMAIL("event.user.update.email"),
-  UPDATE_USER_PASSWORD("event.user.update.password"), RESET_USER_PASSWORD("event.user.reset.password"), AUTHORIZE_USER("event.user.authorize"),
+          "event.user.update.email.confirm"), RESEND_CONFIRM_EMAIL("event.user.resend.confirm.email"), AUTHORIZE_USER("event.user.authorize"),
   AUTHENTICATION_USER("event.user.authentication");
 
   private final String name;
